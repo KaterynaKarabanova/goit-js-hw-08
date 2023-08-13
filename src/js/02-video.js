7b1af32976a2139122dddc83054a6152c4b006e3
Kateryna <ikatya7575@gmaiil.com>
2023-08-13 17:46:28 +0300

    2&3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME_KEY = `videoplayer-current-time`
+const onPlay = throttle((data)=> {
+    // data is an object containing properties specific to that event
+    localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(data.seconds));
+}, 1000);
+
+player.on('timeupdate', onPlay);
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY)).then(function() {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const formEl = document.querySelector(".feedback-form")
+const CURRENT_FORM_KEY = `feedback-form-state`
 
+
+autoInput()
+const userInfo = {};
+const onInput = throttle( () => { 
+    userInfo.email =  formEl.email.value
+    userInfo.message = formEl.message.value
+    //OR sessionStorage//
+    localStorage.setItem(CURRENT_FORM_KEY, JSON.stringify(userInfo))
+}, 500)
+formEl.addEventListener("input", onInput)
+ 
+function autoInput() {
+    //OR sessionStorage //
+  const saveTextInput =localStorage.getItem(CURRENT_FORM_KEY);
+    if (saveTextInput) {
+     const storageKey = JSON.parse(saveTextInput);
+
+     formEl.email.value = storageKey.email;
+     formEl.message.value = storageKey.message;
+  }
+}
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault()
+     userInfo.email =  formEl.email.value
+     userInfo.message = formEl.message.value
+  console.log(userInfo)  
+    //OR sessionStorage// 
+    localStorage.removeItem(CURRENT_FORM_KEY);
+  e.currentTarget.reset();
+})
